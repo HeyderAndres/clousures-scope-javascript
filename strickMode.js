b2e4aa20dffb58d28fc01c9221d33f520c4f4f78
Heider Arellano Oviedo <90737937+HeyderAndres@users.noreply.github.com>
2026-01-02 08:11:37 -0500

    [Feat] strict mode

---
+++ strickMode.js
@@ -0,0 +1,13 @@
+//strict mode
+'use strict';
+
+pi = 3.1416; // genera error porque pi no se ha declarado previamente
+console.log(pi);
+
+
+function myFunction() {
+    'use strict';
+    return name = 'Heider';
+}
+
+console.log(myFunction());
