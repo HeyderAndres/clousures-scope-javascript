44334971745dd2adec47b42938386a1f885da5bc
Heider Arellano Oviedo <90737937+HeyderAndres@users.noreply.github.com>
2026-01-02 08:02:22 -0500

    [Feat] clase de reasignacion y redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,25 @@
+var firstName; // undefined
+firstName = 'Heider';
+console.log(firstName);
+
+var lastName = 'Arellano'; // declarar / asignar
+lastName = 'Oviedo'; // reasignar
+console.log(lastName);
+
+firstName = 'Owen';
+console.log(firstName);
+
+// let
+let fruit = 'Apple'; // declarar / asignar
+fruit = 'kiwi'; // reasignar
+console.log(fruit);
+
+
+//let fruit = 'Banana'; // Error: el identificador 'fruit' ya ha sido declarado
+console.log(fruit);
+
+// const 
+const animal = 'Dog'; // declarar / asignar
+// const animal = 'Snake';
+// animal =  'Cat'; // Error: reasignación no permitida
+console.log(animal);
